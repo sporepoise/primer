9888dee7a30dd2c4bf49047d5b55b43c08b032d2
Austen Medina <austen@Austens-MBP.attlocal.net>
2024-02-07 14:38:22 -0600

    finished calculateTotal function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 export function calculateTotal (items, tax) {
-  // TODO
+  if (tax < 0) {
+    tax = Math.abs(tax)
+  }
+  let total = 0.0
+
+  for(let item of items){
+    
+      if(item.taxable == true && tax > 0) {
+        let tax_amount = item.price*tax
+        total += (item.price) + tax_amount
+   
+       } else {
+         total += item.price
+       }
+    
+  }
+  return total
 }
+
+const items = [
+  {
+    price: 100,
+    taxable: false
+  },
+  {
+    price: 50,
+    taxable: false
+  },
+  {
+    price: 250,
+    taxable: true
+  }
+]
+
+console.log(calculateTotal(items,.0975))
